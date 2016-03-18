93fce984d4c7bbec0d96da9549e02048daea4086
Bruno Ribeiro <brunoribeiroinf@gmail.com>
2016-03-17 22:00:50 -0300

    Update Trabalho03.py

--- Trabalho03.py
+++ Trabalho03.py
@@ -7,7 +7,7 @@ class Ponto:
         self.y = y
 
 def sign(p1, p2, p3):
-    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y)
 
 def dentroTriang(ponto, triangulo):
     (v1, v2, v3) = triangulo
